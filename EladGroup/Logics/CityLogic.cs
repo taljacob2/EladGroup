bd1f902b31ef682741c01ecacd22c9669f6a0e33
Tal Jacob <taljacob2@gmail.com>
2023-02-23 17:15:05 +0200

    Add Inject Of `Startup` To `CityLogic`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- EladGroup/Logics/CityLogic.cs
+++ EladGroup/Logics/CityLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace EladGroup.Logics
 {
     internal class CityLogic
     {
+        private Startup Startup { get; } = Startup.Instance; 
+        
         public void Insert()
         {
 
