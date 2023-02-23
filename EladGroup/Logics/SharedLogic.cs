f574f015b62d08b96fdc6c9b7506d3152ce22f51
Tal Jacob <taljacob2@gmail.com>
2023-02-23 20:39:22 +0200

    Add RunListQuery. Working On FillEntry
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- EladGroup/Logics/SharedLogic.cs
+++ EladGroup/Logics/SharedLogic.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace EladGroup.Logics
 {
-    internal class SharedLogic<T> where T : class
+    internal class SharedLogic<T> where T : class, new()
     {
         protected Startup Startup { get; } = Startup.Instance;
 
@@ -28,6 +28,54 @@ namespace EladGroup.Logics
             {
                 Console.WriteLine(e);
             }
+        }
+
+        private T FillEntry(SqlDataReader reader)
+        {
+            T t = new T();
+
+            Int32.TryParse(reader["Id"].ToString(), out int id);
+            t.Id = id;
+
+            t.Name = reader["Name"].ToString();
+
+            Int32.TryParse(reader["Priority"].ToString(), out
+                int priority);
+            city.Priority = priority;
+
+            return t;
+        }
+        
+        protected List<T> RunListQuery(string query)
+        {
+            List<T> returnValue = new List<T>();
+
+            using (SqlConnection sqlConnection =
+                new SqlConnection(Startup.ConnectionInitiator.ConnectionString))
+            {
+                SqlCommand cmd =
+                    new SqlCommand(query, sqlConnection);
+                try
+                {
+                    sqlConnection.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            returnValue.Add(FillEntry(reader));
+                        }
+
+                        sqlConnection.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            return returnValue;
         }
     }
 }
