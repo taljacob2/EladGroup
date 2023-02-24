93f7d9ee2fc71f6870c5243c026462a7bab9ef60
Tal Jacob <taljacob2@gmail.com>
2023-02-25 01:15:16 +0200

    Fix ps1
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- EladGroup/Program.cs
+++ EladGroup/Program.cs
@@ -1,13 +1,15 @@
-﻿namespace EladGroup
+﻿using EladGroup.Misc.PowerShells;
+
+namespace EladGroup
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
             // TODO: Check ps1 script path.
-            // startup.InitDatabase();
+            Startup.Instance.InitDatabase();
 
-            new MainConsole().Run();
+            // new MainConsole().Run();
         }
     }
 }
