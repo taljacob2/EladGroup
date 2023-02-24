6fd99a48797b0be4cda8638f47ea730b20b3d104
Tal Jacob <taljacob2@gmail.com>
2023-02-24 13:29:22 +0200

    Implement StreetConsole Insert
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- EladGroup/Logics/StreetLogic.cs
+++ EladGroup/Logics/StreetLogic.cs
@@ -10,8 +10,25 @@ namespace EladGroup.Logics
 {
     internal class StreetLogic : SharedLogic<Street>
     {
+        private const int StreetNameMaxCharCount = 50;
+        
+        /// <summary>
+        /// Inserts a new <see cref="Street"/> entity to the database.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="priority"></param>
+        /// <param name="cityId"></param>
+        /// <exception cref="Exception">In case `Street.Name`'s length is too long.</exception>
+        /// <see cref="StreetNameMaxCharCount"/>
         public void Insert(string name, int priority, int cityId)
         {
+            if (name.Length > StreetNameMaxCharCount)
+            {
+                throw new Exception("`Street.Name`'s length is too long");
+            }
+            
+            // TODO: Check if `cityId` is an existing cityId.
+            
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.Append(
                 "INSERT INTO Street (Name, Priority, CityId) VALUES ");
--- EladGroup/_Console/StreetConsole.cs
+++ EladGroup/_Console/StreetConsole.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using EladGroup.Logics;
 using EladGroup.Misc;
+using EladGroup.Models;
 
 namespace EladGroup._Console
 {
@@ -8,11 +9,38 @@ namespace EladGroup._Console
     {
         private StreetLogic StreetLogic { get; } = new StreetLogic();
 
+        /// <summary>
+        /// Inserts a new <see cref="Street"/> entity to the database, by
+        /// receiving inputs from the user via a console interface.
+        /// </summary>
+        /// <exception cref="Exception">In case failed to parse `Street.Priority`</exception>
+        /// <exception cref="Exception">In case `Street.Name`'s length is too long.</exception>
         public void Insert()
         {
-            // TODO: need to implement.
+            string input = null;
+
+            Console.Write("Street.Name: ");
+            input = Console.ReadLine();
+            string name = input;
+
+            Console.Write("Street.Priority: ");
+            input = Console.ReadLine();
+            if (!Int32.TryParse(input, out int priority))
+            {
+                throw new Exception("Failed to parse `Street.Priority`");
+            }
+
+            Console.Write("Street.CityId: ");
+            input = Console.ReadLine();
+            if (!Int32.TryParse(input, out int cityId))
+            {
+                throw new Exception("Failed to parse `Street.CityId`");
+            }
+
+            Console.WriteLine("Inserting to db...");
+            StreetLogic.Insert(name, priority, cityId);
         }
-        
+
         public void Get()
         {
             StreetLogic.Get().ForEach(street =>
