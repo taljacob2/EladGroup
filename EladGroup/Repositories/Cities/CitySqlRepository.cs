2dce2b23fe42bd86f057c093faf16e989da8293e
Tal Jacob <taljacob2@gmail.com>
2023-02-24 15:09:56 +0200

    Reorder Code
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- EladGroup/Repositories/Cities/CitySqlRepository.cs
+++ EladGroup/Repositories/Cities/CitySqlRepository.cs
@@ -9,6 +9,22 @@ namespace EladGroup.Repositories.Cities
     internal class CitySqlRepository : SharedSqlRepository<City>,
         ICityRepository
     {
+        protected override City FillEntry(SqlDataReader reader)
+        {
+            City city = new City();
+
+            int.TryParse(reader["Id"].ToString(), out int id);
+            city.Id = id;
+
+            city.Name = reader["Name"].ToString();
+
+            int.TryParse(reader["Priority"].ToString(), out
+                int priority);
+            city.Priority = priority;
+
+            return city;
+        }
+
         /// <summary>
         ///     Inserts a new <see cref="City" /> entity to the database.
         /// </summary>
@@ -32,22 +48,6 @@ namespace EladGroup.Repositories.Cities
         public List<City> GetOrderByPriority()
         {
             return RunListQuery("SELECT * FROM City ORDER BY Priority");
-        }
-
-        protected override City FillEntry(SqlDataReader reader)
-        {
-            City city = new City();
-
-            int.TryParse(reader["Id"].ToString(), out int id);
-            city.Id = id;
-
-            city.Name = reader["Name"].ToString();
-
-            int.TryParse(reader["Priority"].ToString(), out
-                int priority);
-            city.Priority = priority;
-
-            return city;
         }
     }
 }
