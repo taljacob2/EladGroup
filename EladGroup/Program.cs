f9915e40daa4d8febaa569d8594488d93e9e8bff
Tal Jacob <taljacob2@gmail.com>
2023-02-25 22:08:17 +0200

    After Merge `fix-init-database`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- EladGroup/Program.cs
+++ EladGroup/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            Startup.InitDatabase();
+            // Startup.InitDatabase();
 
             new MainConsole().Run();
         }
